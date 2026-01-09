827f6ffd0a8362eb90856de0d4d46efaeb9e6aa9
Zyxary <kelpselp@gmail.com>
2026-01-09 19:17:27 +0800

    Using argparse to handle inputs

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+
 
 def main():
     try:
@@ -11,10 +13,14 @@ def main():
         printf("Error: env var was not found");
 
     client = genai.Client(api_key=api_key)
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
   
     response = client.models.generate_content(
-    model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
+    model='gemini-2.5-flash', contents=f"{args}")
+
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count} ")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
