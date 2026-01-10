a14f15f575803444bea6029663e7f20bdc787c94
Zyxary <kelpselp@gmail.com>
2026-01-10 15:18:40 +0800

    Writing to files

---
+++ test_write_file.py
@@ -0,0 +1,12 @@
+import os
+from functions.write_file import write_file
+
+results = [
+    write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"),
+    write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"),
+    write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+]
+
+
+for result in results:
+    print(result)
